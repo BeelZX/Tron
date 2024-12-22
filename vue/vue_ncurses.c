f76b734bbec24ff045204daceab521c8cc8282d3
Dylan <dydy86100@gmail.com>
2024-12-22 22:03:32 +0100

    création  de 2 fonctions et ajout des includes

---
+++ vue/vue_ncurses.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "./modele/modele.h"
+
+
+
+// Affichage du menu des choix suivants: J1 vs Ordi, ou J1 vs J2 
+bool displayMenuBot() {
+    
+}
+
+// Affiche le menu principal pour la version Ncurses
+bool displayMenu() {
+
+}
+
+int main(void) {
+    return EXIT_SUCCESS;
+}
