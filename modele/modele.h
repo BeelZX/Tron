551a833f3b789896a7f8a00a5a85462441c9373f
Dylan <dydy86100@gmail.com>
2024-12-23 22:29:08 +0100

    modifications des structures
    ajout des pointeurs pour les variables dont leur variables en avaient besoin

--- modele/modele.h
+++ modele/modele.h
@@ -3,36 +3,36 @@
 #include <stdbool.h>
 
 typedef enum {
-    UP = 0,
-    RIGHT,
-    DOWN,
-    LEFT
+    UP = 0,                 // Vers le haut
+    DOWN = 1,               // Vers le bas
+    LEFT = 2,               // Vers la gauche
+    RIGHT = 3               // Vers la droite
 } Direction;
 
 typedef struct {
-    int x;               // Position x de la moto
-    int y;               // Position y de la moto
-    Direction direction; // Direction (haut, bas, gauche, ou droite)
-    char controls[4];    // Les touches directionnelles
+    int x;                  // Position x de la moto
+    int y;                  // Position y de la moto
+    Direction direction;    // Direction (haut, bas, gauche, ou droite)
+    char controls[4];       // Les touches directionnelles
 } Bike;
 
 typedef struct {
-    int width;          // Largeur du plateau  
-    int height;         // Hauteur du plateau
-    short **grid;       // Tableau représentant le plateau
+    int width;              // Largeur du plateau  
+    int height;             // Hauteur du plateau
+    short **grid;           // Tableau représentant le plateau
 } Board;
 
 typedef struct {
-    Bike bike;          // La moto du joueur 
-    int score;          // Le score du joueur
-    bool IsAlive;       // True si la moto est en vie, false sinon
+    Bike *bike;              // La moto du joueur 
+    int score;              // Le score du joueur
+    bool isAlive;           // True si la moto est en vie, false sinon
 } Player;
 
 typedef struct {
-    Board board;        // Plateau du jeu
-    Player *players;    // Tableau contenant tous les joueurs
-    int nbPlayers;      // Nombre de joueurs
-    bool isGameOver;    // True si partie est finie, false sinon
+    Board *board;            // Plateau du jeu
+    Player **players;        // Tableau contenant tous les joueurs
+    int nbPlayers;          // Nombre de joueurs
+    bool isGameOver;        // True si partie est finie, false sinon
 } Game;
 
 #endif 
