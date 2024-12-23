bf7561a731a5518ab3b3dcbd8dc4674056061ae7
Dylan <dydy86100@gmail.com>
2024-12-23 22:30:47 +0100

    ajout des includes et modifications de plusieurs fonctions
    ajout des pointeurs sur le variables qui en avaient, modifications et déplacement des commentaires et fonctions

--- modele/modele.c
+++ modele/modele.c
@@ -1,39 +1,77 @@
-#include "/model.h"
+#include "./modele.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
 
-Board init_board(int width, int height) {
-    Board board;
-    board.width = width;
-    board.height = height;
-
-    board.grid = malloc(height * sizeof(short *));
+// Initialise le plateau
+Board *init_board(int width, int height) {
+    Board *board = malloc(sizeof(Board));
+    if(!board) exit(EXIT_FAILURE);
+    board->width = width;
+    board->height = height;
+    board->grid = (int **) malloc(height * sizeof(short *));
+    if(!board->grid) exit(EXIT_FAILURE);
     for (int i = 0; i < height; i++) {
-        board.grid[i] = calloc(width, sizeof(short));
+        board->grid[i] = (int *) calloc(width, sizeof(short));
+        if(!board->grid[i]) exit(EXIT_FAILURE);
     }
     return board;
-}// Initialise le plateau
+}
 
+// Initialise le joueur
+Player *init_player(int x, int y, Direction direction, const char controls[4]) {
+    Player *player = (Player *) malloc(sizeof(Player));
+    if(!player) exit(EXIT_FAILURE);
+    player->bike = (Bike *) malloc(sizeof(Bike));
+    if(!player->bike) exit(EXIT_FAILURE);
+    player->bike->x = x;
+    player->bike->y = y;
+    player->bike->direction = direction;
+    player->score = 0;
+    player->isAlive = true;
+    player->bike->direction = (char *) malloc(sizeof(char) * 4);
+    if(!player->bike->direction) exit(EXIT_FAILURE);
+    memcpy(player->bike->controls, controls, sizeof(char) * 4);         // Copie des touches pour jouer dans le tableau de bike
+    return player;
+}
+
+// Initialise le jeu
+Game *init_game(int boardWidth, int boardHeight){
+    Game *game = (Game *) malloc(sizeof(game));
+    if(!game) exit(EXIT_FAILURE);
+    game->nbPlayers = 2;
+    game->isGameOver = false;
+    game->players = (Player **) malloc(sizeof(Player) * game->nbPlayers);
+    if(!game->players) exit(EXIT_FAILURE);
+    game->board = (Board *) init_board(boardWidth, boardHeight);
+    return game;
+}
+
+// Libère la mémoire allouée pour le plateau
 void free_board(Board *board) {
     for (int i = 0; i < board->height; i++) {
         free(board->grid[i]);
     }
     free(board->grid);
-    board->grid = NULL;
-}// libere la memoire du plateau
+    free(board);
+}
 
-Player init_player(int x, int y, Direction direction, const char controls[4]) {
-    Player player;
-    player.bike.x = x;
-    player.bike.y = y;
-    player.bike.direction = direction;
-    player.score = 0;
-    player.isAlive = true;
-    memcpy(player.bike.controls, controls, sizeof(char) * 4); // Les touches pour jouer
+// Libère la mémoire allouée pour tous les joueurs
+void free_players(Player **players, int nb_players) {
+    for (int i = 0; i < nb_players; i++) {
+        free(players[i]);
+    }
+}
 
-    return player;
-}// Initialise le joueur
+// Libère la mémoire allouée pour le jeu
+void free_game(Game *game) {
+    free_board(game->board);
+    free_players(game->players, game->nbPlayers);
+    free(game);
+}
 
+// Déplacement de la moto
 void move_bike(Bike *bike) {
     switch (bike->direction) {
         case UP:
@@ -49,26 +87,24 @@ void move_bike(Bike *bike) {
             bike->x--;
             break;
     }
-}// Deplacement de la moto
+}
 
+// Change la direction de la moto, retourne true si changement possible et fait, false sinon
+bool change_direction(Bike *bike, Direction direction) {
+    
+}
+
+// Vérifie s'il y a une collision avec le bord du plateau ou avec un obstacle
 bool check_collision(){
 
-}// Vérifie si il y a une collision avec le bord du plateau ou avec un obstacle
+}
 
+// Place une trace derrière la moto sur le plateau
 void leave_trace(){
 
-}// la trace derriere la moto
+}
 
-Game init_game(){
-
-}// Initialisation du jeu
-
+// Vérifie si le jeu est fini
 bool check_game_over(Game *game) {
 
-}// Vérifie si le jeu est fini
-
-void free_game(Game *game) {
-    free_board(&game->board);
-    free(game->players);
-    game->players = NULL;
-}// libere la memoire du jeu
+}
