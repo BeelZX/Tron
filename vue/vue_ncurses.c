a33f9d6df9e384023eade740c4307917c6733dde
Dylan <dydy86100@gmail.com>
2024-12-23 22:28:21 +0100

    suppression de la fonction displayMenuBot

--- vue/vue_ncurses.c
+++ vue/vue_ncurses.c
@@ -3,13 +3,6 @@
 #include <stdbool.h>
 #include "./modele/modele.h"
 
-
-
-// Affichage du menu des choix suivants: J1 vs Ordi, ou J1 vs J2 
-bool displayMenuBot() {
-    
-}
-
 // Affiche le menu principal pour la version Ncurses
 bool displayMenu() {
 
